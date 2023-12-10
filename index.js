d8d198627d7f0e3d0d3041d11d6c05df635eb7f7
kubrakk <105593907+kubrakkirill@users.noreply.github.com>
2023-12-10 22:26:28 +0100

    Mistake fixed

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const dollar = 40;
 alert(`1$ = ${dollar}`)
-let result = [];
+const result = [];
 for(let i = 10; i <= 100; i += 10){
     result.push(`${i}$ = ${dollar * i}`)
 }
 alert(result)
 
 const userInput = prompt("Enter a number:");
-const number = parseInt(userInput);
-
-if (isNaN(number)) {
+if (isNaN(userInput)) {
     alert("Not a number");
+} else if(!Number.isInteger(parseFloat(userInput))){
+    alert("Not an integer");
 } else {
     let isPrime = true;
+    const number = parseFloat(userInput)
 
     if (number <= 1) {
         isPrime = false;
@@ -25,28 +26,32 @@ if (isNaN(number)) {
         }
     }
     if (isPrime) {
-        alert(`${number} is a simple number.`);
+        alert(`${userInput} is a simple number.`);
     } else {
-        alert(`${number} not a simple number.`);
+        alert(`${userInput} not a simple number.`);
     }
 }
 
 const anotherInput = prompt("Enter a number:");
-const anotherNumber = parseInt(anotherInput);
 
-if (isNaN(anotherNumber)) {
+if (isNaN(anotherInput)) {
     alert("Not a number");
+} else if(!Number.isInteger(parseFloat(anotherInput))){
+    alert("Not an integer");
 } else {
-    let a = {result: false, pow: 0};
+    const anotherNumber = parseFloat(anotherInput)
+    const result = { is3inDegree: false, pow: 0 }
     for (let i = 0; Math.pow(3, i) <= anotherNumber; i++) {
         if (Math.pow(3, i) === anotherNumber) {
-            a = {result: true, pow: i};
-            break;
+        result.is3inDegree = true;
+        result.pow = i;
+        break;
         }
     }
-    if (a.result) {
-        alert(`${anotherNumber} можна отримати зведенням числа 3 в ${a.pow} ступінь.`);
+    if (result.is3inDegree) {
+        alert(`${anotherInput} можна отримати зведенням числа 3 в ${result.pow} ступінь.`);
     } else {
-        alert(`${anotherNumber} не можна отримати зведенням числа 3 в цілу ступінь.`);
+        alert(`${anotherInput} не можна отримати зведенням числа 3 в цілу ступінь.`);
     }
 }
+
